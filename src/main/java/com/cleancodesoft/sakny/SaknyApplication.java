afedbe2bdd4d4c9b4950299737f578647e6966bf
Mohamed Mekhemer <mmekhemer3@gmail.com>
2026-01-24 11:24:26 +0200

    Initial Spring Boot project setup

---
+++ src/main/java/com/cleancodesoft/sakny/SaknyApplication.java
@@ -0,0 +1,13 @@
+package com.cleancodesoft.sakny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaknyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaknyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cleancodesoft/sakny/SaknyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cleancodesoft.sakny;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaknyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
